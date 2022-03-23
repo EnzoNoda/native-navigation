c5f485bf29778c262a3740e84c9c02ff19cc5c15
Enzo Noda <enzo.noda@gmail.com>
2022-03-22 23:39:51 -0300

    commit

--- src/routes/AppRotas.js
+++ src/routes/AppRotas.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import Home from '../telas/Home';
-import MelhoresProdutores from '../telas/MelhoresProdutores';
+import ProdutorRotas from './ProdutorRotas';
+import MelhoresProdRotas from './MelhoresProdRotas';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,8 +11,8 @@ export default function AppRotas() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Melhores Produtores" component={MelhoresProdutores} />
+        <Tab.Screen name="Home" component={ProdutorRotas} />
+        <Tab.Screen name="Melhores Produtores" component={MelhoresProdRotas} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ src/routes/MelhoresProdRotas.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProdutorRotas from './ProdutorRotas';
+import MelhoresProdutores from '../telas/MelhoresProdutores';
+
+export default function MelhoresProdRotas() {
+  return <ProdutorRotas componentePrincipal={MelhoresProdutores} />;
+}
